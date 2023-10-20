386ddaad0dbceb0510ed44db0df8bed4ff7cb6dd
Bhaktij Koli <bhaktijkoli121@gmail.com>
2023-10-20 18:48:09 +0530

    feat: keycloak init

--- src/index.js
+++ src/index.js
@@ -1,10 +1,41 @@
 require('dotenv').config()
+const Keycloak = require('keycloak-connect');
 const express = require('express')
+const session = require('express-session');
 const app = express()
 const port = process.env.PORT || 3000
 
+const memoryStore = new session.MemoryStore();
+
+var sessionStore = {
+  secret: 'demo',
+  resave: false,
+  saveUninitialized: true,
+  store: memoryStore,
+  cookie: {
+    secure: false,
+  }
+}
+
+const keycloak = new Keycloak({
+  store: memoryStore
+});
+
+app.use(session(sessionStore))
+
+app.use(keycloak.middleware());
+
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Open Endpoint')
+})
+
+app.all('/callback', (req, res) => {
+  res.send('Callback Endpoint')
+})
+
+app.get('/protected', keycloak.protect(), (req, res) => {
+  res.send('Protected Endpoint')
 })
 
 app.listen(port, () => {
